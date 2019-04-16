0981a5dd07b24139867cc61b867544d07e4be1a6
github username <github email address>
2019-04-16 10:33:12 -0400

    update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: user_params[:username])
     if !!@user
       token = encode_token({ user_id: @user.id })
-      render json: { user: {username: @user.username}, token: token }, status: :found
+      render json: { user: {username: @user.username, user_id: @user.id, name: @user.name, picture: @user.picture, hometown: @user.hometown, current_city: @user.current_city, age: @user.age, bio: @user.bio}, token: token }, status: :found
     else
       render json: { message: 'Invalid username or password' }, status: :not_acceptable
     end
@@ -40,8 +40,14 @@ class UsersController < ApplicationController
     @user = User.find(id)
 
     if @user.valid?
-      render json: { user: {username: @user.username}}
+      render json: { user: {username: @user.username, user_id: @user.id, name: @user.name, picture: @user.picture, hometown: @user.hometown, current_city: @user.current_city, age: @user.age, bio: @user.bio}}
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(name: params[:user][:name], picture: params[:user][:picture], hometown: params[:user][:hometown], current_city: params[:user][:current_city], age: params[:user][:age], bio: params[:user][:bio])
+    render json: @user
   end
 
   private
